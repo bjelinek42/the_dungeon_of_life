8deb0cc53fd6f159cde744a1dd2053e327f4f46a
ben <bjelinek42@gmail.com>
2022-06-16 15:43:34 -0600

    computer player for tictacktoe

--- tic_tack_toe.rb
+++ tic_tack_toe.rb
@@ -1,19 +1,20 @@
 class Tic_tack_toe
   def initialize
     @board = {"a1" => " ", "a2" => " ", "a3" => " ", "b1" => " ", "b2" => " ", "b3" => " ", "c1" => " ", "c2" => " ", "c3" => " "}
-    @winner = false
+    @winner = ""
     @count = 0
+    @move = 0
   end
 
   def begin_tic_tack_toe
     display_board
     while true
-      if @winner == true
+      if @winner != ""
         break
       end
       player1_move
       display_board
-      if @winner == true || @count == 9
+      if @winner != "" || @count == 9
         break
       end
       player2_move
@@ -42,37 +43,59 @@ class Tic_tack_toe
           puts "Please enter a valid position"
         end
       else
-        puts "Position already taken"
+        puts "Please enter a valid position"
       end
     end
     @count += 1
     if @board["a1"] == "X" && @board["a2"] == "X" && @board["a3"] == "X" || @board["b1"] == "X" && @board["b2"] == "X" && @board["b3"] == "X" || @board["c1"] == "X" && @board["c2"] == "X" && @board["c3"] == "X" || @board["a1"] == "X" && @board["b2"] == "X" && @board["c3"] == "X" || @board["c1"] == "X" && @board["b2"] == "X" && @board["a3"] == "X" || @board["a1"] == "X" && @board["b1"] == "X" && @board["c1"] == "X" || @board["a2"] == "X" && @board["b2"] == "X" && @board["c2"] == "X" || @board["a3"] == "X" && @board["b3"] == "X" && @board["c3"] == "X" 
       puts "Player 1 wins!"
-      @winner = true
+      @winner = "Player 1"
     elsif @count == 9
       puts "Cat's game!"
     end
   end
 
   def player2_move
-    puts "Player 2 (O), choose a position (e.g. a1, b2, c3)"
-    while true
-      position = gets.chomp
-      if @board[position] == " "
-        if position == "a1" || position == "a2" || position == "a3" || position == "b1" || position == "b2" || position == "b3" || position == "c1" || position == "c2" || position == "c3" 
-          @board["#{position}"] = "O"
-          break
-        else
-          puts "Please enter a valid position"
-        end
+    puts "Suzies turn!"
+    if @move == 0
+      if @board["a3"] == " "
+        @board["a3"] = "O"
       else
-        puts "Position already taken"
+        @board["c1"] = "O"
+      end
+    elsif @move == 1
+      if @board["c1"] == " "
+        @board["c1"] = "O"
+      elsif @board["a1"] == " "
+        @board["a1"] = "O"
+      else
+        @board["c3"] = "O"
+      end
+    elsif @move >= 2
+      if @board["a3"] == "O" && @board["c1"] == "O" && @board["b2"] == " "
+        @board["b2"] = "O"
+      elsif @board["a1"] == "O" && @board["c1"] == "O" && @board["b1"] == " "
+        @board["b1"] = "O"
+      elsif @board["a3"] == "O" && @board["c3"] == "O" && @board["b3"] == " "
+        @board["b3"] == "O"
+      else
+        possible_moves = []
+        @board.each do |position, char|
+          if char == " "
+            possible_moves << position
+          end
+        end
+        @board[possible_moves.sample] = "O"
       end
     end
+    @move += 1
     @count += 1
     if @board["a1"] == "O" && @board["a2"] == "O" && @board["a3"] == "O" || @board["b1"] == "O" && @board["b2"] == "O" && @board["b3"] == "O" || @board["c1"] == "O" && @board["c2"] == "O" && @board["c3"] == "O" || @board["a1"] == "O" && @board["b2"] == "O" && @board["c3"] == "O" || @board["c1"] == "O" && @board["b2"] == "O" && @board["a3"] == "O" || @board["a1"] == "X" && @board["b1"] == "X" && @board["c1"] == "X" || @board["a2"] == "X" && @board["b2"] == "X" && @board["c2"] == "X" || @board["a3"] == "X" && @board["b3"] == "X" && @board["c3"] == "X" 
-      puts "Player 2 wins!"
-      @winner = true
+      puts "Suzie wins!"
+      @winner = "Suzie"
     end
   end
 end
+
+game = Tic_tack_toe.new
+game.begin_tic_tack_toe
